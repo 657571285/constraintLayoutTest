48ef50d94ce44698c477b60c8c7c1a336f184a97
colorful <657571285@qq.com>
2017-02-09 10:28:57 +0800

    test2

--- app/src/main/java/com/colorful/constraintlayouttest/MainActivity.java
+++ app/src/main/java/com/colorful/constraintlayouttest/MainActivity.java
@@ -1,7 +1,13 @@
 package com.colorful.constraintlayouttest;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt_login = (Button) findViewById(R.id.login);
+        Button bt_regist = (Button) findViewById(R.id.regist);
+        EditText et_zhanghao  = (EditText) findViewById(R.id.et_zhanghao);
+        EditText et_password  = (EditText) findViewById(R.id.et_password);
+        final String zhanghao  = et_zhanghao.getText().toString().trim();
+        final String password  = et_password.getText().toString().trim();
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(zhanghao.equals("a")&&password.equals("a")){
+                    Toast.makeText(getApplicationContext(),"登录成功",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
